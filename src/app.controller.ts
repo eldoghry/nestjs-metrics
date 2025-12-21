8807ebba88937b735224373932cda08113a2840b
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-12-21 10:50:18 +0200

    simulate flow

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  ForbiddenException,
+  Get,
+  HttpException,
+  Param,
+  Query,
+  ServiceUnavailableException,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { getRandomInt, getRandomItem, sleep } from './helper';
+import { get } from 'http';
 
 @Controller()
 export class AppController {
@@ -7,6 +18,56 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/success')
+  getSuccess(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/error')
+  getError(
+    @Query() errorCode: number,
+    @Query() random: boolean = false,
+  ): string {
+    let randomErrorCode: number | undefined;
+
+    if (random) {
+      const errorCodes = [400, 403, 503, 500];
+      randomErrorCode = getRandomItem<number>(errorCodes);
+      throw new HttpException(
+        `Simulated random error ${randomErrorCode}`,
+        randomErrorCode as number,
+      );
+    } else {
+      randomErrorCode = errorCode;
+    }
+
+    switch (randomErrorCode) {
+      case 400:
+        throw new BadRequestException('Bad Request Exception');
+      case 403:
+        throw new ForbiddenException('Forbidden Exception');
+      case 503:
+        throw new ServiceUnavailableException('Service Unavailable Exception');
+      default:
+        throw new Error('Simulated error');
+    }
+  }
+
+  @Get('/consume')
+  async getConsumeTime(
+    @Query() durationInSeconds: number,
+    @Query() random: boolean = false,
+  ): Promise<string> {
+    if (random) {
+      const randomDuration = getRandomInt(1, 30);
+      await sleep(randomDuration * 1000);
+    } else {
+      await sleep(durationInSeconds ? durationInSeconds * 1000 : 3000);
+    }
+
     return this.appService.getHello();
   }
 }
---
+++ src/helper.ts
@@ -0,0 +1,16 @@
+export function sleep(ms: number = 3000): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+export function getRandomInt(min: number, max: number): number {
+  const minCeil = Math.ceil(min);
+  const maxFloor = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloor - minCeil + 1)) + minCeil;
+}
+
+export function getRandomItem<T>(arr: T[]): T | undefined {
+  if (arr.length === 0) return undefined;
+
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
