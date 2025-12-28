bb0f5df496d0e5d1f673edda1f71bb71a63daf8f
Mohamed Magdy <moh.mag.ali@gmail.com>
2025-12-28 12:13:14 +0200

    use pino logger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,22 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { AxiosService } from './axios/axios.service';
+import { PinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class AppService {
-  private readonly logger = new Logger(AppService.name);
+  // private readonly logger = new Logger(AppService.name);
 
-  constructor(private readonly axiosService: AxiosService) {}
+  constructor(
+    private readonly axiosService: AxiosService,
+    private readonly logger: PinoLogger,
+  ) {
+    this.logger.setContext(AppService.name);
+  }
 
   getHello(): string {
-    this.logger.log('step: 1 - getHello called');
-    this.logger.log('step: 2 - simulating some processing work');
-    this.logger.log('step: 3 - returning Hello World');
+    this.logger.info('step: 1 - getHello called');
+    this.logger.info('step: 2 - simulating some processing work');
+    this.logger.info('step: 3 - returning Hello World');
     return 'Hello World!';
   }
 
